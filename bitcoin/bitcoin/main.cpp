4b29e6db09fc6b7798ef4c16037a35aaec0c1df1
Larry Lai <cgpanda.sg@gmail.com>
2017-12-01 16:22:58 +0800

    For reading the code much easier

---
+++ bitcoin/bitcoin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bitcoin
+//
+//  Created by Larry Lai on 1/12/17.
+//  Copyright © 2017 Super Fun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
